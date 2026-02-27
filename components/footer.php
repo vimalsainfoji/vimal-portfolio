4d544b01c7b82caa835e0f42fba7b3188cd98ff9
vimalsainfoji <gamerfact195@gmail.com>
2026-02-27 13:04:18 +0530

    Add footer component with social links and scripts

---
+++ components/footer.php
@@ -0,0 +1,18 @@
+<footer class="footer text-center">
+  <div class="container">
+    <div class="mb-3">
+      <a href="#"><i class="fab fa-github me-3"></i></a>
+      <a href="#"><i class="fab fa-linkedin me-3"></i></a>
+      <a href="#"><i class="fab fa-instagram"></i></a>
+    </div>
+    <p>© <span id="year"></span> Vimal Kumar. All Rights Reserved.</p>
+  </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script src="js/script.js"></script>
+
+</body>
+</html>
